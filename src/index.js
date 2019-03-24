6e199ee2b83c1f10e758b254e3194fb77be3ca11
Felice Forby <forby.1@gmail.com>
2019-03-24 11:00:55 +0900

    done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
-// Code Goes Here
+<VideoPlayer>
+  <VideoHeader>
+    <h1 className="video-title">The Simpsons</h1>
+  </VideoHeader>
+  <VideoControls />
+</VideoPlayer>
+
+export default class MovieBrowser extends React.Component {
+  render() {
+    const currentPlayingTitle = 'Mad Max: Fury Road';
+    const childrenWithExtraProp = React.Children.map(this.props.children, child => {
+      return React.cloneElement(child, {
+        isPlaying: child.props.title === currentPlayingTitle
+      });
+    });
+ 
+    return (
+      <div className="movie-browser">
+        {childrenWithExtraProp}
+      </div>      
+    );
+  }
+}
+
+export default class Panel extends React.Component {
+  render() {
+    return (
+      <div className="panel">
+        <div className="panel-header">{this.props.title}</div>
+        <div className="panel-body">{this.props.children}</div>
+      </div>
+    );
+  }
+}
+
+export default class SomeComponent extends React.Component {
+  render() {
+    const childrenWithWrapperDiv = React.Children.map(this.props.children, child => {
+      return (
+        <div className="some-component-special-class">{child}</div> 
+      );
+    });
+ 
+    return (
+      <div className="some-component">
+        <p>This component has {React.Children.count(this.props.children)} children.</p>
+        {childrenWithWrapperDiv}        
+      </div>      
+    );
+  }
+}
